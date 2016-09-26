99ac1ab88e7783cac240a7d996e35430cf6db8e4
qianjin <qj@dareway.com.cn>
2016-09-26 16:14:39 +0800

    增加FrameLayoutExample

---
+++ linearLayoutExample/src/main/java/sde/sdu/edu/helloworld/FrameLayoutExample.java
@@ -0,0 +1,13 @@
+package sde.sdu.edu.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrameLayoutExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout_example);
+    }
+}
