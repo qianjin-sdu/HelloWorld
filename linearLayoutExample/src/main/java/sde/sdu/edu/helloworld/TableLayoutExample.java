6425af22e1f5754d983898fe7f535586dadbd110
qianjin <qj@dareway.com.cn>
2016-09-26 16:00:18 +0800

    增加TableLayoutExample

---
+++ linearLayoutExample/src/main/java/sde/sdu/edu/helloworld/TableLayoutExample.java
@@ -0,0 +1,13 @@
+package sde.sdu.edu.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableLayoutExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_layout_example);
+    }
+}
